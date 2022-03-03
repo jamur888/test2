6e9dce6423733c509a84e40bb871eea601fbaa78
Jamshit <muradow.jamshit@mail.ru>
2022-03-03 22:37:13 +0300

    addClass

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD");
         System.out.println("salam");
-        System.out.println("привет");
+        System.out.println("прит");
     }
 }
