2d6e52cac5890074f5c40b2cbdd8e02434840790
jamur888 <79403715+jamur888@users.noreply.github.com>
2022-03-03 12:54:55 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("salam aleykum");
-        System.out.println("привет");
+        System.out.println("приветуля");
     }
 }
